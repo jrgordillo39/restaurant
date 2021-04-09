eae276d14c1b8593dbd5b144890c94b2ba2ddf3a
jrgordillo39 <jjroberto.9@gmail.com>
2021-04-09 12:45:10 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.636adf7ade116762b6e435cab81673de.js"
+  "./precache-manifest.6e6b608a309dbc7ed939acc665d1dc7f.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "Cubamax-Restaurant"});
+workbox.core.setCacheNameDetails({prefix: "Cubamax"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
